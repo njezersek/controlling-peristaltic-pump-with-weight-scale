b9f9d15d27f3c64a57be16c633db7f8d812e1e8f
Nejc Jezeršek <njezersek@gmail.com>
2022-04-03 13:07:35 +0200

    faster upload with direct port manipulation

--- src/main.cpp
+++ src/main.cpp
@@ -4,61 +4,94 @@
 #define CS 2    // RS
 #define SID 3    // RW
 
-inline void toggle_clk() {
-  digitalWrite(CLK, HIGH);
-  digitalWrite(CLK, LOW);
+#define CLK_MASK 0b01000000 // port B
+#define CS_MASK 0b10000010 // port D
+#define SID_MASK 0b00000001 // port D
+
+inline void pulseCLK() {
+	PORTB |= CLK_MASK; // set CLK high
+	PORTB &= ~CLK_MASK; // set CLK low
 }
 
 void sendCommand(bool rs, bool rw, uint8_t command){
-	digitalWrite(CS, HIGH);
-	digitalWrite(SID, LOW);
-	digitalWrite(CLK, LOW);
+	
 	// sync wiht 5 consecutive 1
-	digitalWrite(SID, HIGH);
 	for(int i = 0; i < 5; i++){
-		digitalWrite(CLK, HIGH);
-		digitalWrite(CLK, LOW);
+		PORTD |= SID_MASK; // set SID high
+		pulseCLK();
 	}
+
 	// send rw
-	digitalWrite(SID, rw);
-	digitalWrite(CLK, HIGH);
-	digitalWrite(CLK, LOW);
+	if(rw) PORTD |= SID_MASK; // set SID high
+	else PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
+	
 	// send rs
-	digitalWrite(SID, rs);
-	digitalWrite(CLK, HIGH);
-	digitalWrite(CLK, LOW);
+	if(rs) PORTD |= SID_MASK; // set SID high
+	else PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
+
 	// send a zero
-	digitalWrite(SID, LOW);
-	digitalWrite(CLK, HIGH);
-	digitalWrite(CLK, LOW);
+	PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
 
 	// send hight 4 bits of command
 	for(int i = 0; i < 4; i++){
-		digitalWrite(SID, (command >> 7) & 1);
-		digitalWrite(CLK, HIGH);
-		digitalWrite(CLK, LOW);
+		if(command & 0b10000000) PORTD |= SID_MASK; // set SID high
+		else PORTD &= ~SID_MASK; // set SID low
+		pulseCLK();
 		command <<= 1;
 	}
 	// send 4 consecutive 0
-	digitalWrite(SID, LOW);
 	for(int i = 0; i < 4; i++){
-		digitalWrite(CLK, HIGH);
-		digitalWrite(CLK, LOW);
+		PORTD &= ~SID_MASK; // set SID low
+		pulseCLK();
 	}
+
 	// send low 4 bits of command
 	for(int i = 0; i < 4; i++){
-		digitalWrite(SID, (command >> 7) & 1);
-		digitalWrite(CLK, HIGH);
-		digitalWrite(CLK, LOW);
+		if(command & 0b10000000) PORTD |= SID_MASK; // set SID high
+		else PORTD &= ~SID_MASK; // set SID low
+		pulseCLK();
 		command <<= 1;
 	}
+
 	// send 4 consecutive 0
-	digitalWrite(SID, LOW);
 	for(int i = 0; i < 4; i++){
-		digitalWrite(CLK, HIGH);
-		digitalWrite(CLK, LOW);
+		PORTD &= ~SID_MASK; // set SID low
+		pulseCLK();
 	}
-	digitalWrite(CS, LOW);
+}
+
+void sendCommand8(bool rs, bool rw, uint8_t command){
+	
+	// sync wiht 5 consecutive 1
+	for(int i = 0; i < 5; i++){
+		PORTD |= SID_MASK; // set SID high
+		pulseCLK();
+	}
+
+	// send rw
+	if(rw) PORTD |= SID_MASK; // set SID high
+	else PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
+	
+	// send rs
+	if(rs) PORTD |= SID_MASK; // set SID high
+	else PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
+
+	// send a zero
+	PORTD &= ~SID_MASK; // set SID low
+	pulseCLK();
+
+	// send hight 4 bits of command
+	for(int i = 0; i < 8; i++){
+		if(command & 0b10000000) PORTD |= SID_MASK; // set SID high
+		else PORTD &= ~SID_MASK; // set SID low
+		pulseCLK();
+		command <<= 1;
+	}
 }
 
 void setup() {
@@ -68,29 +101,45 @@ void setup() {
 	pinMode(CS, OUTPUT);
 	pinMode(SID, OUTPUT);
 
+	digitalWrite(CS, HIGH);
+	digitalWrite(SID, LOW);
+	digitalWrite(CLK, LOW);
+
 	Serial.begin(9600);
 
 	delay(40); // lcd power on delay
 }
 
 void loop() {
-	sendCommand(0,0, 0b00100110); // function set (001D_XRG0: D=8/4b interface, R=exdended instruction set, G=graphic mode)
+	sendCommand(0,0, 0b00110110); // function set (001D_XRG0: D=8/4b interface, R=exdended instruction set, G=graphic mode)
 	delayMicroseconds(72);
-	sendCommand(0,0, 0b00000001); // clear display
-	delay(10);
+	// sendCommand(0,0, 0b00000001); // clear display
+	// delay(10);
 	// sendCommand(0,0, 0b00001111); // show cursor
 	// sendCommand(0,0, 0b00011000); // cursor and display control
 	unsigned long start = micros();
-	for(uint8_t j = 0; j < 32; j++){
+	for(uint8_t j = 0; j < 32; j++){ // rows
 		sendCommand(0,0, 0b10000000 | j); // set GRAM row addres
 		sendCommand(0,0, 0b10000000); // set GRAM column addres
-		for(uint8_t i=0; i<32; i++){
-			if((i+j) % 2 == 0){
-				sendCommand(1,0, 0xff); // write to ram
+		delayMicroseconds(72);
+		for(uint8_t i=0; i<32; i++){ // columns
+			if((i+(j/8)) % 2 == 0){
+				if(i == 0 || i == 16){
+					sendCommand(1,0, 0xbf); // write to ram
+				}
+				else{
+					sendCommand(1,0, 0xff); // write to ram
+				}
 			}
 			else{
-				sendCommand(1,0, 0x00); // write to ram
+				if(i == 0 || i == 16){
+					sendCommand(1,0, 0x80); // write to ram
+				}
+				else{
+					sendCommand(1,0, 0x00); // write to ram
+				}
 			}
+			delayMicroseconds(10);
 		}
 	}
 	unsigned long end = micros();
